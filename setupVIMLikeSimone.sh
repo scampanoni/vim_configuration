80f05110397a03b3b3315957875bcee1361fc4f9
Simone Campanoni <simo.xan@gmail.com>
2020-06-16 14:22:00 -0500

    Improved the installation script

--- setupVIMLikeSimone.sh
+++ setupVIMLikeSimone.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+# Copy vimrc
+cp vimrc ~/.vimrc ;
+
+# Clone YouCompleteMe
 rm -rf ~/.vim/bundle/YouCompleteMe ;
 git clone https://github.com/ycm-core/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
 
-cp vimrc ~/.vimrc ;
+# Install YouCompleteMe
+cd ~/.vim/bundle/YouCompleteMe
+python3 install.py --all
