f860736432994eb84f3872727e88f8db37b1bca6
Simone Campanoni <simo.xan@gmail.com>
2020-06-16 14:24:20 -0500

    Bug fixed

--- setupVIMLikeSimone.sh
+++ setupVIMLikeSimone.sh
@@ -4,9 +4,15 @@
 cp vimrc ~/.vimrc ;
 
 # Clone YouCompleteMe
+pushd ./ ;
 rm -rf ~/.vim/bundle/YouCompleteMe ;
 git clone https://github.com/ycm-core/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
+cd ~/.vim/bundle/YouCompleteMe
+git submodule update --init --recursive
+popd ; 
 
 # Install YouCompleteMe
+pushd ./ ;
 cd ~/.vim/bundle/YouCompleteMe
 python3 install.py --all
+popd ;
