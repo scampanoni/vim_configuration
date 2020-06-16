93eddf4ab70d20ea8d9cf6a4db372ca2493b90fc
Simone Campanoni <simo.xan@gmail.com>
2020-06-16 14:37:51 -0500

    Avoiding deleting existing installations

--- setupVIMLikeSimone.sh
+++ setupVIMLikeSimone.sh
@@ -4,13 +4,23 @@
 cp vimrc ~/.vimrc ;
 
 # Clone Vundle
-rm -rf ~/.vim/bundle/Vundle.vim ;
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim ;
+pushd ./ ;
+if ! test -d ~/.vim/bundle/Vundle.vim ; then 
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim ;
+else
+  cd ~/.vim/bundle/Vundle.vim ;
+  git pull ;
+fi
+popd ;
 
 # Clone YouCompleteMe
 pushd ./ ;
-rm -rf ~/.vim/bundle/YouCompleteMe ;
-git clone https://github.com/ycm-core/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
+if ! test -d ~/.vim/bundle/YouCompleteMe ; then
+  git clone https://github.com/ycm-core/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
+else 
+  cd ~/.vim/bundle/YouCompleteMe ;
+  git pull ;
+fi
 cd ~/.vim/bundle/YouCompleteMe
 git submodule update --init --recursive
 popd ; 
