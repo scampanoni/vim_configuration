860daf35238e46c00683f3a62ef8b645de19a234
Simone Campanoni <simo.xan@gmail.com>
2020-06-16 14:28:41 -0500

    Added vundle

--- setupVIMLikeSimone.sh
+++ setupVIMLikeSimone.sh
@@ -3,6 +3,9 @@
 # Copy vimrc
 cp vimrc ~/.vimrc ;
 
+# Clone Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim ;
+
 # Clone YouCompleteMe
 pushd ./ ;
 rm -rf ~/.vim/bundle/YouCompleteMe ;
